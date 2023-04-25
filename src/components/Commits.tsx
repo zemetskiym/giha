d85e0d12c2ddb1cf89d8eadc284d0dbaaf1f6847
Matthew Zemetskiy <zemetskiym@proton.me>
2023-04-24 19:56:05 -0700

    Feat: new Commits function

---
+++ src/components/Commits.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/components/Commits.module.css"
+
+export default function Commits(): JSX.Element {
+    return (
+        <div>
+            <h1>Commits</h1>
+        </div>
+    )
+} 
