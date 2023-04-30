8eb9a7275c9bacfb234cd8d581a2c7b8a40c5915
Matthew Zemetskiy <zemetskiym@proton.me>
2023-04-30 13:25:59 -0700

    Feat: Commits empty results array to store d3 data

--- src/components/Commits.tsx
+++ src/components/Commits.tsx
@@ -1,6 +1,6 @@
 import styles from "../styles/components/Commits.module.css"
 import * as d3 from "d3";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 // Define types/interfaces
 interface File {
@@ -25,6 +25,9 @@ export default function Commits(props: Props): JSX.Element {
     const {commitData = []} = props
     const filteredCommitData = commitData.filter(Boolean) as Array<Commit>
 
+    // Declare an empty array to store results
+    const [results, setResults] = useState<Array<any>>([])
+
     function cumulativeStackedAreaChart(): JSX.Element {
         // Create a reference to the SVG element that will be rendered.
         const svgRef = useRef<SVGSVGElement>(null);
@@ -32,7 +35,7 @@ export default function Commits(props: Props): JSX.Element {
         // Use the useEffect hook to execute code after the component is mounted or updated.
         useEffect(() => {
             // Remove any null values from the results array.
-            const dataWithoutNull = filteredCommitData.filter((item) => item !== null).sort((a, b) => a.date - b.date);
+            const dataWithoutNull = results.filter((item) => item !== null).sort((a, b) => a.date - b.date);
 
             // Define the dimensions of the chart and its margins.
             const height = 600;
