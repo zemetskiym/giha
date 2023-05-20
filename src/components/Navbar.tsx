2652cadb0ddfcc1db452f4645a893cda5537ebb5
Matthew Zemetskiy <zemetskiym@proton.me>
2023-05-20 15:02:18 -0700

    Feat: make Navbar refresh page for new report

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,17 @@
 import styles from "../styles/components/Navbar.module.css"
 
 export default function Navbar (): JSX.Element {
+    // Function to reload the page
+    function pageReload (): void {
+        window.location.reload()
+    }
+
     return (
-        <>
-        </>
+        <nav>
+            <ul>
+                <li>Commits Analyzer</li>
+                <li onClick={() => pageReload()}>New Report</li>
+            </ul>
+        </nav>
     )
 }
