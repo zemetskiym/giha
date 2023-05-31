d5ccf895dde2eb85ea038547485c64f0b6558833
Matthew Zemetskiy <zemetskiym@proton.me>
2023-05-31 16:31:46 -0700

    Feat: add FunFacts LOC Bar Chart axes and bars

--- src/components/FunFacts.tsx
+++ src/components/FunFacts.tsx
@@ -85,12 +85,11 @@ export default function Commits(props: Props): JSX.Element {
         return sum / filteredCommitData.length;
     };
 
+    // Function to return the day of the week with the most commits
     enum ReturnType {
         Day = 'day',
         Count = 'count'
     };
-
-    // Function to return the day of the week with the most commits
     function findMostProductiveDayOfWeek(filteredCommitData: Array<Commit>, returnType: ReturnType = ReturnType.Day): string | Record<string, number> {
         // Initialize count object
         let count: Record<string, number> = {Sunday: 0, Monday: 0, Tuesday: 0, Wednesday: 0, Thursday: 0, Friday: 0, Saturday: 0};
@@ -144,7 +143,7 @@ export default function Commits(props: Props): JSX.Element {
     };
 
     // D3.js line graph for the number of commits over the time of week
-    function timeOfWeekLineGraph(): JSX.Element {
+    function TimeOfWeekLineGraph(): JSX.Element {
         // Check if the required data is available.
         const hasData = filteredCommitData.length > 0;
         if (!hasData) {
@@ -200,10 +199,22 @@ export default function Commits(props: Props): JSX.Element {
                 .attr("fill", "none");
 
             // Add the x-axis to the chart.
-            svg.append('g')
+            if (windowSize.width < 800) {
+                svg.append('g')
+                .attr('transform', `translate(0,${height - margin.bottom})`)
+                .call(xAxis)
+                .selectAll('text')
+                .style('text-anchor', 'end')
+                .attr('transform', 'rotate(-45)')
+                .attr('dx', '-.8em')
+                .attr('dy', '.15em');
+            } else {
+                svg.append('g')
                 .attr('transform', `translate(0,${height - margin.bottom})`)
                 .call(xAxis);
+            }  
 
+            // Add the inline labels to the chart.
             svg.append("g")
                 .attr("font-family", "sans-serif")
                 .attr("font-size", 10)
@@ -238,6 +249,34 @@ export default function Commits(props: Props): JSX.Element {
         return <svg ref={svgRef} width={Math.min(windowSize.width / 2, 600)} height={Math.min(windowSize.width / 2, 600)} />;
     }
 
+    function findRange(filteredCommitData: Array<Commit>) {
+        // Array of range objects with their corresponding min and max values
+        const ranges = [
+          { range: "0-10", min: 0, max: 10, amount: 0 },
+          { range: "11-30", min: 11, max: 30, amount: 0 },
+          { range: "31-50", min: 31, max: 50, amount: 0 },
+          { range: "51-100", min: 51, max: 100, amount: 0 },
+          { range: "101-200", min: 101, max: 200, amount: 0 },
+          { range: "201-500", min: 201, max: 500, amount: 0 },
+          { range: "500+", min: 501, max: Infinity, amount: 0 },
+        ];
+      
+        // Iterate over the commits
+        for (let commit in filteredCommitData) {
+            const number = filteredCommitData[commit].files[0].patch.split("\n").length;
+            // Iterate over the ranges to find the matching range for the given number
+            for (const rangeObj of ranges) {
+                // Check if the number falls within the min and max values of the range
+                if (number >= rangeObj.min && number <= rangeObj.max) {
+                rangeObj.amount++; // Increment the amount of commits in the range
+                };
+            };
+        };
+
+        // Return the ranges with their corresponding amounts
+        return ranges.map(({ range, amount }) => ({ range, amount }));
+    }
+
     function LOCBarChart(): JSX.Element {
         // Check if the required data is available.
         const hasData = filteredCommitData.length > 0;
@@ -255,11 +294,89 @@ export default function Commits(props: Props): JSX.Element {
             // Clear the SVG by removing all existing elements.
             svg.selectAll('*').remove();
 
+            const LOCData = findRange(filteredCommitData);
+
             // Define the dimensions of the chart and its margins.
             const height = Math.min(windowSize.width / 2, 600);
             const width = Math.min(windowSize.width / 2, 600);
             const margin = {top: 20, right: 20, bottom: 42, left: 20};
 
+            // Define the halo color and width.
+            const halo = "#fff";
+            const haloWidth = 6;
+
+            // Define the x-axis scale.
+            const xScale = d3.scaleBand()
+                .domain(["0-10", "11-30", "31-50", "51-100", "101-200", "201-500", "500+"])
+                .rangeRound([margin.left, width - margin.right])
+                .padding(0.1);
+
+            // Define the x-axis with tick lines and no axis line.
+            const xAxis = (g: d3.Selection<SVGGElement, unknown, null, undefined>) => g
+                .call(d3.axisBottom(xScale));
+
+            // Define the y-axis scale.
+            const yScale = d3.scaleLinear()
+                .domain([0, d3.max(LOCData, (d) => d.amount)!])
+                .rangeRound([height - margin.bottom, margin.top]);
+
+            // Add a rect for each bar.
+            svg.append("g")
+                .attr("fill", "steelblue")
+                .selectAll()
+                .data(LOCData)
+                .join("rect")
+                .attr("x", (d) => xScale(d.range)!)
+                .attr("y", (d) => yScale(d.amount))
+                .attr("height", (d) => yScale(0) - yScale(d.amount))
+                .attr("width", xScale.bandwidth());
+
+            // Add the x-axis to the chart.
+            if (windowSize.width < 800) {
+                svg.append('g')
+                .attr('transform', `translate(0,${height - margin.bottom})`)
+                .call(xAxis)
+                .selectAll('text')
+                .style('text-anchor', 'end')
+                .attr('transform', 'rotate(-45)')
+                .attr('dx', '-.8em')
+                .attr('dy', '.15em');
+            } else {
+                svg.append('g')
+                .attr('transform', `translate(0,${height - margin.bottom})`)
+                .call(xAxis);
+            };
+
+            // Add the inline labels to the chart.
+            svg.append("g")
+                .attr("font-family", "sans-serif")
+                .attr("font-size", 10)
+                .attr("text-anchor", "middle")
+                .attr("stroke-linejoin", "round")
+                .attr("stroke-linecap", "round")
+                .selectAll("text")
+                .data(LOCData)
+                .join("text")
+                .attr("dy", "0.35em")
+                .attr("x", (d) => xScale(d.range)! + xScale.bandwidth() / 2)
+                .attr("y", (d) => yScale(d.amount) - 10) // Adjust the y-coordinate for label placement
+                .text((d) => d.amount)
+                .call(text => text
+                    .filter((_, j, I) => j === I.length - 1)
+                    .append("tspan")
+                      .attr("font-weight", "bold")
+                      .text(" Commits"))
+                .call(text => text.clone(true))
+                .attr("fill", "none")
+                .attr("stroke", halo)
+                .attr("stroke-width", haloWidth);
+
+            // Set the base font size.
+            const baseFontSize = 16; // in pixels
+
+            // Set text font size.
+            svg.selectAll("text")
+                .style("font-size", `${12 / baseFontSize}rem`);
         }, [svgRef, windowSize])
 
         return <svg ref={svgRef} width={Math.min(windowSize.width / 2, 600)} height={Math.min(windowSize.width / 2, 600)} />
@@ -272,7 +389,8 @@ export default function Commits(props: Props): JSX.Element {
             <div>I consistently follow the <code>{countProgrammingConventions(filteredCommitData)}</code> programming convention</div>
             <div>My most productive days are {findMostProductiveDayOfWeek(filteredCommitData) as string}s</div>
             <div>I commit my code in {findMostProductiveTimeOfDay(filteredCommitData)}s</div>
-            <div>{timeOfWeekLineGraph()}</div>
+            <span>{TimeOfWeekLineGraph()}</span>
+            <span>{LOCBarChart()}</span>
         </>
     );
 };
