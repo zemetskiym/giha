b99a05c2df6f71ab958c98ca25853cf1b506a942
Matthew Zemetskiy <zemetskiym@proton.me>
2023-06-12 17:00:27 -0700

    Feat: Navbar NextAuth signin

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import styles from "../styles/components/Navbar.module.css"
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Navbar (): JSX.Element {
     // Function to reload the page
@@ -6,10 +7,23 @@ export default function Navbar (): JSX.Element {
         window.location.reload()
     }
 
+    // Retrieving the user's NextAuth.js session data
+    const { data: session } = useSession()
+
     return (
         <nav>
             <ul>
                 <li>Commits Analyzer</li>
+
+                {/* Rendering the sign in button if the user is not signed in */}
+                {!session && <li onClick={() => signIn()} className={styles.navLink}>
+                    Sign in
+                </li>}
+                {/* Rendering the sign out button if the user is signed in */}
+                {session && <li onClick={() => signOut()} className={styles.navLink}>
+                    Sign out
+                </li>}
+                
                 <li onClick={() => pageReload()}>New Report</li>
             </ul>
         </nav>
