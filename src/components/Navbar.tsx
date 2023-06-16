5d0198b5530ada27e4491f61b87cdd16ab8e038f
Matthew Zemetskiy <zemetskiym@proton.me>
2023-06-16 13:15:51 -0700

    Feat: add Navbar person and plus icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
-import styles from "../styles/components/Navbar.module.css"
-import { useSession, signIn, signOut } from "next-auth/react"
+import styles from "../styles/components/Navbar.module.css";
+import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
 
 export default function Navbar (): JSX.Element {
     // Function to reload the page
@@ -17,14 +18,19 @@ export default function Navbar (): JSX.Element {
 
                 {/* Rendering the sign in button if the user is not signed in */}
                 {!session && <li onClick={() => signIn()} className={styles.navLink}>
-                    Sign in
+                    <Image src="/icons/person.svg" alt="" width={24} height={24} />
+                    <span>Sign in</span>
                 </li>}
                 {/* Rendering the sign out button if the user is signed in */}
                 {session && <li onClick={() => signOut()} className={styles.navLink}>
-                    Sign out
+                    <Image src="/icons/person.svg" alt="" width={24} height={24} />
+                    <span>Sign out</span>
                 </li>}
                 
-                <li onClick={() => pageReload()}>New Report</li>
+                <li onClick={() => pageReload()}>
+                    <Image src="/icons/plus.svg" alt="" width={24} height={24} />
+                    <span>New Report</span>
+                </li>
             </ul>
         </nav>
     )
