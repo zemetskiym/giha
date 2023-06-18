853ea7ebbde14edf0d0411479fa57124a2dfcbb3
Matthew Zemetskiy <zemetskiym@proton.me>
2023-06-17 19:41:12 -0700

    Feat: Navbar add styles and adaptive design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,37 +1,79 @@
 import styles from "../styles/components/Navbar.module.css";
 import { useSession, signIn, signOut } from "next-auth/react";
 import Image from "next/image";
+import { useWindowSizeContext } from '@/components/context';
+import { useState } from "react";
 
 export default function Navbar (): JSX.Element {
+    // Define state for the mobile menu
+    const [mobileMenu, setMobileMenu] = useState(false);
+
+    // Retrieving the user's NextAuth.js session data
+    const { data: session } = useSession()
+
+    // Retrieving the window size
+    const windowSize = useWindowSizeContext();
+
     // Function to reload the page
     function pageReload (): void {
         window.location.reload()
     }
 
-    // Retrieving the user's NextAuth.js session data
-    const { data: session } = useSession()
-
     return (
-        <nav>
-            <ul>
-                <li>Commits Analyzer</li>
+        <nav id={styles.navbar}>
+            {windowSize.width >= 500 && 
+                <ul id={styles.navbarList}>
+                    <li id={styles.title}>Commits Analyzer</li>
 
-                {/* Rendering the sign in button if the user is not signed in */}
-                {!session && <li onClick={() => signIn()} className={styles.navLink}>
-                    <Image src="/icons/person.svg" alt="" width={24} height={24} />
-                    <span>Sign in</span>
-                </li>}
-                {/* Rendering the sign out button if the user is signed in */}
-                {session && <li onClick={() => signOut()} className={styles.navLink}>
-                    <Image src="/icons/person.svg" alt="" width={24} height={24} />
-                    <span>Sign out</span>
-                </li>}
-                
-                <li onClick={() => pageReload()}>
-                    <Image src="/icons/plus.svg" alt="" width={24} height={24} />
-                    <span>New Report</span>
-                </li>
-            </ul>
+                    {/* Rendering the sign in button if the user is not signed in */}
+                    {!session && <li onClick={() => signIn()} className={styles.navLink}>
+                        <Image id={styles.personIcon} className={styles.icon} src="/icons/person.svg" alt="" width={14} height={14} />
+                        <span className={styles.marginRight}>Sign in</span>
+                    </li>}
+                    {/* Rendering the sign out button if the user is signed in */}
+                    {session && <li onClick={() => signOut()} className={styles.navLink}>
+                        <Image id={styles.personIcon} className={styles.icon} src="/icons/person.svg" alt="" width={14} height={14} />
+                        <span className={styles.marginRight}>Sign out</span>
+                    </li>}
+                    
+                    <li onClick={() => pageReload()} className={styles.navLink}>
+                        <Image id={styles.plusIcon} className={styles.icon} src="/icons/plus.svg" alt="" width={14} height={14} />
+                        <span>New Report</span>
+                    </li>
+                </ul>
+            }
+            {windowSize.width < 500 && 
+                <>
+                <ul id={styles.evenNavbarList}>
+                    <li onClick={() => setMobileMenu(prev => !prev)} className={styles.navLink}>
+                        <Image className={styles.icon} src="/icons/hamburger-menu.svg" alt="Menu" width={24} height={24} />
+                    </li>
+                    <li onClick={() => pageReload()} className={styles.navLink}>
+                        <Image className={styles.icon} src="/icons/plus.svg" alt="" width={24} height={24} />
+                    </li>
+                </ul>
+                {mobileMenu && 
+                <ul id={styles.dropdownMenu}>
+                    <hr className={styles.hr} />
+                    {/* Rendering the sign in button if the user is not signed in */}
+                    {!session && <li onClick={() => signIn()} className={styles.navLink}>
+                        <Image id={styles.personIcon} className={styles.icon} src="/icons/person.svg" alt="" width={14} height={14} />
+                        <span className={styles.marginRight}>Sign in</span>
+                    </li>}
+                    {/* Rendering the sign out button if the user is signed in */}
+                    {session && <li onClick={() => signOut()} className={styles.navLink}>
+                        <Image id={styles.personIcon} className={styles.icon} src="/icons/person.svg" alt="" width={14} height={14} />
+                        <span className={styles.marginRight}>Sign out</span>
+                    </li>}
+                    <hr className={styles.hr} />
+                    <li onClick={() => pageReload()} className={styles.navLink}>
+                        <Image id={styles.plusIcon} className={styles.icon} src="/icons/plus.svg" alt="" width={14} height={14} />
+                        <span>New Report</span>
+                    </li>
+                </ul>
+                }
+                </>
+            }
         </nav>
     )
 }
