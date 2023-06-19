0eac9a2d8a2c62b1667a89c200ae0c2c71a8ea28
Matthew Zemetskiy <zemetskiym@proton.me>
2023-06-19 12:39:07 -0700

    Refactor: remove pie chart horizontal padding

--- src/components/Languages.tsx
+++ src/components/Languages.tsx
@@ -338,7 +338,7 @@ export default function Languages(props: Props): JSX.Element {
 
                 // Define the dimensions of the chart and its margins.
                 const height = Math.min(windowSize.width / 2, 600);
-                const width = Math.min(windowSize.width, 1200);
+                const width = Math.min(windowSize.width / 2, 600);
 
                 // Define the radius of the chart.
                 const padAngle = 0;
@@ -392,7 +392,7 @@ export default function Languages(props: Props): JSX.Element {
         }, [results, pieChartSvgRef, windowSize]);
 
         // Return the SVG element with the specified dimensions.
-        if (hasData) return <svg ref={pieChartSvgRef} width={Math.min(windowSize.width, 1200)} height={Math.min(windowSize.width / 2, 600)} />;
+        if (hasData) return <svg ref={pieChartSvgRef} width={Math.min(windowSize.width / 2, 600)} height={Math.min(windowSize.width / 2, 600)} />;
         if (!hasData) return <p>There is not enough data available to visualize the chart. Please try again later.</p>
         return <p>There is not enough data available to visualize the chart. Please try again later.</p>
     }
