715f7a23b8463a733c0534462ece574b67183122
Matthew Zemetskiy <zemetskiym@proton.me>
2023-07-28 18:58:47 -0700

    Feat: add Sidebar component and style file

---
+++ src/components/dashboard/Sidebar.tsx
@@ -0,0 +1,8 @@
+import styles from "../../styles/components/dashboard/Sidebar.module.css"
+
+export default function Sidebar(): JSX.Element {
+    return (
+        <nav>
+        </nav>
+    )
+}
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,9 @@
+import Sidebar from "../components/dashboard/Sidebar.tsx"
+
 export default function Dashboard() {
     return (
-        <></>
+        <>
+            <Sidebar />
+        </>
     )
 }
