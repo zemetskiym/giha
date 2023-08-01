24e8a5e40010ad00e0b1f142a7102625ecedf74d
zemetskiym <zemetskiym@proton.me>
2023-07-31 17:56:34 -0700

    Fix: dashboard ensure user login

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,13 @@
 import Sidebar from "../components/dashboard/Sidebar.tsx"
+import { useSession, signIn } from "next-auth/react";
 
 export default function Dashboard() {
+    // Retrieving the user's NextAuth.js session data
+    const { data: session } = useSession();
+
+    // Signing in the user if the user has not done so already
+    if (!session) signIn("github");
+
     return (
         <>
             <Sidebar />
