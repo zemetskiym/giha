7cf4323d83496a378392a2cc3ad6428b205092a6
zemetskiym <zemetskiym@proton.me>
2023-08-05 14:51:21 -0700

    Feat: Footer add experimental dashboard link

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -8,6 +8,7 @@ export default function Languages(): JSX.Element {
             <div id={styles.links}>
                 <Link className={styles.link} target="_blank" href="https://github.com/zemetskiym/giha/blob/main/LICENSE">License</Link>
                 <Link className={styles.link} target="_blank" href="https://github.com/zemetskiym/giha">GitHub Repository</Link>
+                <Link className={styles.link} href="/dashboard">Dashboard (Experimental)</Link>
             </div>
             <div id={styles.copyright}>© {new Date().getFullYear()} Matthew Zemetskiy. Licensed under the MIT License.</div>
         </footer>
