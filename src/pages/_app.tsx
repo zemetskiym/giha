248187bc636606adc8c70aabc578c5942c22ed03
zemetskiym <zemetskiym@proton.me>
2023-08-12 17:54:40 -0700

    Fix: wrap page in darkModeContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { WindowSizeProvider } from '@/components/context'
+import { DarkModeProvider } from '@/components/darkModeContext'
 import { SessionProvider } from "next-auth/react"
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
       <WindowSizeProvider>
-        <Component {...pageProps} />
+        <DarkModeProvider>
+            <Component {...pageProps} />
+        </DarkModeProvider>
       </WindowSizeProvider>
     </SessionProvider>
   )
