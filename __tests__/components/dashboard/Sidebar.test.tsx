a29e0926d72d8752a8b8fd32a5cd3f2393c8926c
zemetskiym <zemetskiym@proton.me>
2023-08-15 21:46:55 -0700

    Test: Sidebar mock useSession and render beforeEach

--- __tests__/components/dashboard/Sidebar.test.tsx
+++ __tests__/components/dashboard/Sidebar.test.tsx
@@ -1,9 +1,11 @@
 import '@testing-library/jest-dom/extend-expect';
 import React from "react";
-import { render, fireEvent, waitFor } from "@testing-library/react";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import Sidebar from "../../../src/components/dashboard/Sidebar";
 import { DarkModeProvider } from '../../../src/components/darkModeContext';
+import { useSession } from "next-auth/react";
+jest.mock("next-auth/react");
 
 // Define the types for the React state and state setter
 type ActiveViewType = string;
@@ -18,23 +20,23 @@ describe("Sidebar Component", () => {
     beforeEach(() => {
         mockActiveView = 'dashboard';
         mockSetActiveView = jest.fn();
-    });
 
-    it('should redirect to "/" when the home button is clicked', () => {
-        // Mock the window.innerWidth to simulate a larger screen (1024 pixels)
-        global.innerWidth = 1024;
+        // Mock useSession with no session value
+        (useSession as jest.Mock).mockReturnValue([false, false]);
 
-        // Render the Sidebar component inside MemoryRouter to enable testing navigation
-        const { getByTestId } = render(
+        // Render the Sidebar component
+        render(
             <MemoryRouter>
                 <DarkModeProvider>
                     <Sidebar activeView={mockActiveView} setActiveView={mockSetActiveView} />
                 </DarkModeProvider>
-            </MemoryRouter>
+            </MemoryRouter> 
         );
+    });
 
+    it('should redirect to "/" when the home button is clicked', () => {
         // Get the home button by test ID
-        const homeButton = getByTestId('home');
+        const homeButton = screen.getByTestId('home');
 
         // Click the home button
         fireEvent.click(homeButton);
@@ -44,15 +46,7 @@ describe("Sidebar Component", () => {
     });
 
     it('should change activeView state when the analysis button is clicked', () => {
-        // Mock the window.innerWidth to simulate a larger screen (1024 pixels)
-        global.innerWidth = 1024;
-
-        const { getByTestId } = render(
-            <DarkModeProvider>
-                <Sidebar activeView={mockActiveView} setActiveView={mockSetActiveView} />
-            </DarkModeProvider>
-        );
-        const analysisButton = getByTestId('analysis');
+        const analysisButton = screen.getByTestId('analysis');
 
         fireEvent.click(analysisButton);
 
@@ -61,66 +55,33 @@ describe("Sidebar Component", () => {
     });
 
     it('should link to the Github new issue page with an issue button', () => {
-        // Mock the window.innerWidth to simulate a larger screen (1024 pixels)
-        global.innerWidth = 1024;
-
-        const { getByTestId } = render(
-            <DarkModeProvider>
-                <Sidebar activeView={mockActiveView} setActiveView={mockSetActiveView} />
-            </DarkModeProvider>
-        );
-        const issueButton = getByTestId('issue');
+        const issueButton = screen.getByTestId('issue');
 
         // Check if window.open was called with the expected URL
         expect(issueButton).toHaveAttribute('href', 'https://github.com/zemetskiym/giha/issues/new/choose');
     });
 
     it('should link to the Github repository page with a repository button', () => {
-        // Mock the window.innerWidth to simulate a larger screen (1024 pixels)
-        global.innerWidth = 1024;
-
-        const { getByTestId } = render(
-            <DarkModeProvider>
-                <Sidebar activeView={mockActiveView} setActiveView={mockSetActiveView} />
-            </DarkModeProvider>
-        );
-        const repositoryButton = getByTestId('repository');
+        const repositoryButton = screen.getByTestId('repository');
 
         // Check if window.open was called with the expected URL
         expect(repositoryButton).toHaveAttribute('href', 'https://github.com/zemetskiym/giha');
     });
 
     it('should link to the Github license page with a license button', () => {
-        // Mock the window.innerWidth to simulate a larger screen (1024 pixels)
-        global.innerWidth = 1024;
-
-        const { getByTestId } = render(
-            <DarkModeProvider>
-                <Sidebar activeView={mockActiveView} setActiveView={mockSetActiveView} />
-            </DarkModeProvider>
-        );
-        const licenseButton = getByTestId('license');
+        const licenseButton = screen.getByTestId('license');
 
         // Check if window.open was called with the expected URL
         expect(licenseButton).toHaveAttribute('href', 'https://github.com/zemetskiym/giha/blob/main/LICENSE');
     });
 
     it('should change the dark mode state when the dark mode button is clicked', () => {
-        // Mock the window.innerWidth to simulate a larger screen (1024 pixels)
-        global.innerWidth = 1024;
-
-        const { getByTestId } = render(
-            <DarkModeProvider>
-                <Sidebar activeView={mockActiveView} setActiveView={mockSetActiveView} />
-            </DarkModeProvider>
-        );
-
-        const darkModeButton = getByTestId('dark-mode-button');
+        const darkModeButton = screen.getByTestId('dark-mode-button');
 
         fireEvent.click(darkModeButton);
 
         waitFor(() => {
-            const darkModeIcon = getByTestId('dark-mode-icon');
+            const darkModeIcon = screen.getByTestId('dark-mode-icon');
 
             // Assert that the dark mode icon appears after clicking the button, indicating a state change
             expect(darkModeIcon).toBeInTheDocument();
@@ -128,42 +89,22 @@ describe("Sidebar Component", () => {
     }); 
 
     it('should link to the Github mailto email with the email button', () => {
-        // Mock the window.innerWidth to simulate a larger screen (1024 pixels)
-        global.innerWidth = 1024;
-
-        const { getByTestId } = render(
-            <DarkModeProvider>
-                <Sidebar activeView={mockActiveView} setActiveView={mockSetActiveView} />
-            </DarkModeProvider>
-        );
-        const emailButton = getByTestId('email');
+        const emailButton = screen.getByTestId('email');
 
         // Check if window.open was called with the expected URL
         expect(emailButton).toHaveAttribute('href', 'mailto:gihanalysis@proton.me');
     });
 
     it('should open a popout with a signout button when the profile menu button is clicked', () => {
-        // Mock the window.innerWidth to simulate a larger screen (1024 pixels)
-        global.innerWidth = 1024;
-
-        // Render the Sidebar component inside MemoryRouter to enable testing navigation
-        const { getByTestId } = render(
-            <MemoryRouter>
-                <DarkModeProvider>
-                    <Sidebar activeView={mockActiveView} setActiveView={mockSetActiveView} />
-                </DarkModeProvider>
-            </MemoryRouter>
-        )
-
         // Get the profile menu button by test ID
-        const profileMenu = getByTestId('profile-menu');
+        const profileMenu = screen.getByTestId('profile-menu');
 
         // Click the profile menu button
         fireEvent.click(profileMenu);
     
         // Check if the signout button exists
         waitFor(() => {        
-            const signout = getByTestId('profile-menu-signout');
+            const signout = screen.getByTestId('profile-menu-signout');
             expect(signout).toBeInTheDocument();
         });
     });
